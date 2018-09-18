da53c771f2a38459523472010645a41354c95a52
tphdev <tphdevdrop@gmail.com>
2018-09-17 22:34:37 -0500

    cleanup

--- assignment-files/main.js
+++ assignment-files/main.js
@@ -9,27 +9,11 @@ document.querySelector("#validate-field-btn").addEventListener('click',function(
 })
 
 
-document.querySelector("#calculate-items-btn").addEventListener('click', function(e){
+document.querySelector("#calculate-items-btn").addEventListener('click', function(){
+  // TASK #3
 })
 
-document
-.querySelector("#select-to-show-more-btn")
-.addEventListener('click',function addCarInfo(){
- // TASK #4
- var carListDropDownEl = document.querySelector('.selection')
- var index = carListDropDownEl.selectedIndex
- var imgEl = document.querySelector('.img-box')
- var makeAndModelEl = document.querySelector('.model')
- var msrpEl = document.querySelector('.msrp')
- var mpgEl = document.querySelector('.mpg')
- var edmundsEl = document.querySelector('.edmunds')
 
-// Using this variable to read the data of EACH index
- var datasetIndex = carListDropDownEl.options[index].dataset
-
- imgEl.innerHTML = '<img src="./images/'+datasetIndex.img+'">'
- makeAndModelEl.innerHTML = carListDropDownEl.value
- msrpEl.innerHTML = datasetIndex.msrp
- mpgEl.innerHTML = datasetIndex.mpg
- edmundsEl.innerHTML = datasetIndex.edmunds
+document.querySelector("#select-to-show-more-btn").addEventListener('click', function(){
+  // TASK #4
 })
