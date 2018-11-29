4d89c480605cea80b5ab5df244e1bbb963d2e1ed
MiriamGaGuMuk <mgg_pjl@yahoo.com>
2018-11-29 15:56:31 -0600

    Some tasks solved

--- assignment-files/main.js
+++ assignment-files/main.js
@@ -1,16 +1,39 @@
 document.querySelector("#reset-field-btn").addEventListener('click', function(){
       // TASK #1
+
+      let text = document.querySelector('.ex1-text').value;
+
+      if(text !== ""){
+        text = text.reset();
+      }
+
+
   })
 
 
 document.querySelector("#validate-field-btn").addEventListener('click',function(){
   // TASK #2
+  let msgInp = document.querySelector('.ex2-text');
+  let errMsg = document.querySelector('.flash-message');
 
+  if (msg.checked) {
+    rate_value = document.getElementById('r1').value;
+  }
 })
 
 
 document.querySelector("#calculate-items-btn").addEventListener('click', function(){
   // TASK #3
+  let roundInput = document.querySelector('.item-input').value;
+  let greenCheck = document.querySelector("sum-total");
+  let checkedBox = 0;
+
+  if (roundInput.checked) {
+    roundInput = greenCheck;
+    checkedBox = roundInput++;
+  
+  }
+  
 })
 
 
